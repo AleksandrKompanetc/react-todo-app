676e8c21e9f5cb62970d080c6c6bdd3f819a9a6a
AleksandrKompanetc <moneymacker7@gmail.com>
2025-02-04 23:27:36 +0200

    adding styles for container and heading

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,18 @@
 import React from 'react'
 
+const style = {
+  bg: `h-screen w-screen p-4 bg-gradient-to-r from-[#2F80ED] to-[#1CB5E0]`,
+  container: ``,
+  heading: ``
+}
+
 const Todo = () => {
   return (
-    <div>Todo</div>
+    <div className={style.bg}>
+      <div className={style.container}>
+        <h3 className={style.heading}>Todo App</h3>
+      </div>
+    </div>
   )
 }
 
