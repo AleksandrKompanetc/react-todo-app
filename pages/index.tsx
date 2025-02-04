995113227e0a9fac9831658811d566f54e12ec20
AleksandrKompanetc <moneymacker7@gmail.com>
2025-02-04 23:39:54 +0200

    added plus button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { AiOutlinePlus } from 'react-icons/ai'
 
 const style = {
   bg: `h-screen w-screen p-4 bg-gradient-to-r from-[#2F80ED] to-[#1CB5E0]`,
@@ -16,7 +17,7 @@ const Todo = () => {
         <h3 className={style.heading}>Todo App</h3>
         <form className={style.form}>
           <input className={style.input} type="text" placeholder='Add Todo' />
-          <button className={style.button}></button>
+          <button className={style.button}><AiOutlinePlus size={30} /></button>
         </form>
       </div>
     </div>
